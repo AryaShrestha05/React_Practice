9db877605876453630af9108fa247594263fe5b0
AryaShrestha05 <aryashrestha005@gmail.com>
2025-07-03 18:43:26 -0400

    useState and useEffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+//Allows us to do something in some kind of event, effect
+import { useEffect } from 'react';
+import { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    //dependency arrays: Runs everytime the value in the array changes
+    //Runs only once if the array is empty
+    alert("You have changed the counter to: " + count)
+  }, [count])
+  
+    return(
+      <div className = "App">
+        <button onClick={() => {setCount((prevCount) => prevCount - 1)}}>-</button>
+        <h1>{count}</h1>
+        <button onClick = {() => {setCount((prevCount) => prevCount + 1)}}>+</button>
+      </div>
+    )
 }
 
 export default App;
